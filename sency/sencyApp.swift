bffabe0baf7b46a57354367d5942e6a71c4197c8
NosmMani <noam.many93@gmail.com>
2021-08-10 21:06:40 +0300

    Initial Commit

---
+++ sency/sencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  sencyApp.swift
+//  sency
+//
+//  Created by MobileTeam on 10/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct sencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
